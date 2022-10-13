ee699e6466b888c29ead8e803cc7cab75342f6d6
Konkere <amorph@konkere.ru>
2022-10-13 13:07:10 +0300

    Add sum size and sort by count/size

--- ext_counter.py
+++ ext_counter.py
@@ -9,13 +9,29 @@ from datetime import datetime
 
 def args_parser():
     parser = argparse.ArgumentParser(description='Extensions calculation in dir (recursive).')
-    parser.add_argument('-s', '--scan', type=str, help='Dir to scan.', required=False)
+    parser.add_argument('-d', '--dir', type=str, help='Dir to scan.', required=False)
     parser.add_argument('-o', '--output', type=str, help='TXT file with results.', required=False)
+    parser.add_argument('-s', '--sort', type=str, help='Sort by count/size.', required=False)
     arguments = parser.parse_args().__dict__
     return arguments
 
 
-def scanner(path_for_scan, filename):
+def size_converter(size_bytes):
+    x_bytes = {
+        'Gb': 1073741824,
+        'Mb': 1048576,
+        'Kb': 1024,
+        'b': 0,
+    }
+    x_bytes_sorted = sorted(x_bytes.items(), key=lambda x: x[1], reverse=True)
+    for (name, size_1) in x_bytes_sorted:
+        if size_bytes >= size_1:
+            size_round = int(round(size_bytes/size_1 if size_1 > 0 else size_bytes, 0))
+            size_converted = f'{size_round} {name}'
+            return size_converted
+
+
+def scanner(path_for_scan, filename, sort_by):
     extensions = {}
     pattern_ext = r'.+\.([^\.]+)$'
 
@@ -25,9 +41,17 @@ def scanner(path_for_scan, filename):
     if not filename:
         filename = f'extensions_{datetime.now():%Y%m%d%H%M}.txt'
 
+    if not sort_by and sort_by != 'size':
+        sort_by = 'count'
+
     for root, dirs, files in os.walk(path_for_scan):
         for file in files:
             re_extension = re.match(pattern_ext, file)
+            file_path = os.path.join(root, file)
+            try:
+                file_size = os.stat(file_path).st_size
+            except FileNotFoundError:
+                file_size = 0
             try:
                 ext = re_extension.group(1)
             except AttributeError:
@@ -37,17 +61,22 @@ def scanner(path_for_scan, filename):
             try:
                 extensions[ext]
             except KeyError:
-                extensions[ext] = 1
+                extensions[ext] = {
+                    'count': 1,
+                    'size': file_size
+                }
             else:
-                extensions[ext] += 1
+                extensions[ext]['count'] += 1
+                extensions[ext]['size'] += file_size
 
-    sorted_extensions = sorted(extensions.items(), key=lambda x: x[1], reverse=True)
+    sorted_extensions = sorted(extensions.items(), key=lambda x: x[1][sort_by], reverse=True)
 
     with open(filename, "w") as result:
-        for extension, num in sorted_extensions:
-            result.write(f'{num} - {extension}\n')
+        for extension, nums in sorted_extensions:
+            size = size_converter(nums["size"])
+            result.write(f'{extension:<16}\t{nums["count"]:<10}\t{size:<10}\n')
 
 
 if __name__ == '__main__':
     args = args_parser()
-    scanner(path_for_scan=args['scan'], filename=args['output'])
+    scanner(path_for_scan=args['dir'], filename=args['output'], sort_by=args['sort'])
